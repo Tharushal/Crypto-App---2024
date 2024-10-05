19d56ce644adcf1a1ed93c8df470093f4db66a40
Tharusha <tharushalakshan3@gmail.com>
2024-10-05 14:57:36 +0530

    Initial Commit

---
+++ Crypto App/Crypto_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_AppApp.swift
+//  Crypto App
+//
+//  Created by Tharusha Lakshan on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
